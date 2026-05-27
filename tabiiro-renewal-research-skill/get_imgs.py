4479438c7f40bef6f0e23406365c00b8029e42bf
自分の名前 <yusuke26kazu@gmail.com>
2026-05-27 11:22:37 +0900

    Clean up tabiiro renewal skill package

--- tabiiro-renewal-research-skill/get_imgs.py
+++
@@ -1,8 +0,0 @@
-import urllib.request
-import re
-url = 'https://tabiiro.jp/gourmet/theme/french/ranking/kinki/kyoto/'
-req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-html = urllib.request.urlopen(req).read().decode('utf-8')
-imgs = re.findall(r'<img[^>]+src=[\"\']([^\"\']+)[\"\']', html)
-for img in imgs[:30]:
-    print(img)
